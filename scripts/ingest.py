644a3a07f6235d423b2e5f66877cfcb44be71646
cam01h <cam01houston@gmail.com>
2026-05-23 22:08:23 +0100

    "initial effort at aml code extraction to json"

---
+++ scripts/ingest.py
@@ -0,0 +1,58 @@
+import pymupdf4llm
+import json
+from pathlib import Path
+from typing import cast
+
+
+def aml_code_extract(input_path, output_path):
+    print("Extracting AML Code 2019...")
+    md = cast(str, pymupdf4llm.to_markdown(input_path, header=False, footer=False))
+    start_marker = "The Department of Home Affairs makes the following Code under section 157 of the Proceeds of Crime Act 2008 and section 68 of the Terrorism and Other Crime (Financial . Restrictions) Act 2014, after carrying out the consultation required by those sections[1] "
+    md = md.split(start_marker, 1)[1]
+    end_marker = "## **43 Revocations** "
+    md = md.split(end_marker, 1)[0]
+
+    lines = md.splitlines()
+    document = "The AML/CFT Code 2019"
+    hierarchy = "legislation"
+    part = ""
+    paragraph = ""
+    chunks = []
+    buffer = ""
+    for line in lines:
+        if line.startswith("## **PART"):
+            part = line.replace("## **", "").replace("**", "")
+        elif line.startswith("## **"):
+            if buffer != "":
+                current_chunk = {
+                    "document": document,
+                    "hierarchy": hierarchy,
+                    "part": part,
+                    "paragraph": paragraph,
+                    "body": buffer.strip(),
+                }
+                chunks.append(current_chunk)
+                buffer = ""
+            paragraph = line.replace("## **", "").replace("**", "")
+        else:
+            buffer += "\n" + line
+    chunk = {
+        "document": document,
+        "hierarchy": hierarchy,
+        "part": part,
+        "paragraph": paragraph,
+        "body": buffer.strip(),
+    }
+    chunks.append(chunk)
+
+    with output_path.open("w") as f:
+        for chunk in chunks:
+            f.write(json.dumps(chunk) + "\n")
+    print("AML Code 2019 extracted")
+
+
+if __name__ == "__main__":
+    project_root = Path(__file__).parent.parent
+    input = project_root / "data/raw/custom/the_aml_code_2019.pdf"
+    output = project_root / "data/processed/raw_code.jsonl"
+    aml_code_extract(input, output)
